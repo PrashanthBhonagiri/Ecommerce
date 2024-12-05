0212c3269e8926fd1b208b3593a02e213633a91a
PrashanthBhonagiri <prashanth.bhonagiri@gmail.com>
2024-12-06 00:17:22 +0530

    Add ItemController for handling Item-related HTTP requests

---
+++ src/main/java/com/example/Ecommerce/controller/ItemController.java
@@ -0,0 +1,44 @@
+package com.example.Ecommerce.controller;
+
+import com.example.Ecommerce.models.Item;
+import com.example.Ecommerce.service.ItemService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/items")
+public class ItemController {
+    private ItemService itemService;
+
+    @Autowired
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Item> createItem(@RequestBody Item item) {
+        Item createdItem = itemService.createItem(item);
+        return new ResponseEntity<>(createdItem, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getItem(@PathVariable Long id) {
+        Item item = itemService.getItem(id);
+        return ResponseEntity.ok(item);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Item>> getAllItems() {
+        List<Item> items = itemService.getAllItems();
+        return ResponseEntity.ok(items);
+    }
+
+
+
+}
