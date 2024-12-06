e817c189a63ec5fdeef889864a1464ade221d3a8
PrashanthBhonagiri <prashanth.bhonagiri@gmail.com>
2024-12-06 17:07:33 +0530

    Update Admin Controller to fetch stats

--- src/main/java/com/example/Ecommerce/controller/AdminController.java
+++ src/main/java/com/example/Ecommerce/controller/AdminController.java
@@ -1,9 +1,12 @@
 package com.example.Ecommerce.controller;
 
+import com.example.Ecommerce.dto.AdminStats;
 import com.example.Ecommerce.models.DiscountCode;
+import com.example.Ecommerce.service.AdminService;
 import com.example.Ecommerce.service.DiscountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/admin")
 public class AdminController {
     private final DiscountService discountService;
+    private final AdminService adminService;
 
     @Autowired
-    public AdminController(DiscountService discountService) {
+    public AdminController(DiscountService discountService, AdminService adminService) {
         this.discountService = discountService;
+        this.adminService = adminService;
     }
 
     @PostMapping("/generate-discount")
     public ResponseEntity<DiscountCode> generateDiscountCode() {
         DiscountCode discountCode = discountService.generateDiscountCode();
         return ResponseEntity.ok(discountCode);
+    }
+
+    @GetMapping("/stats")
+    public ResponseEntity<AdminStats> getAdminStats() {
+        AdminStats stats = adminService.getAdminStats();
+        return ResponseEntity.ok(stats);
     }
 
 }
